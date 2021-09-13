38a8a1381c0ef7c69a442718d0427e36bc5b661d
Frank Betancur <alejok6@gmail.com>
2021-09-13 17:53:17 -0500

    first commit base line code

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleados',
+  templateUrl: './empleados.component.html',
+  styleUrls: ['./empleados.component.css']
+})
+export class EmpleadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarEmpleado() {
+    console.log('click');
+
+  }
+}
